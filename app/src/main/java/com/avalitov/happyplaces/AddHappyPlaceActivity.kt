daae8601c14ca383d689c5a7653eda55e93254d8
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-09-25 23:39:19 +0500

    Preparations for Add Place Activity

---
+++ app/src/main/java/com/avalitov/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,21 @@
+package com.avalitov.happyplaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place)
+
+        var toolbarAddPlace : Toolbar = findViewById(R.id.tb_add_place)
+        setSupportActionBar(toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
--- app/src/main/java/com/avalitov/happyplaces/MainActivity.kt
+++ app/src/main/java/com/avalitov/happyplaces/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.avalitov.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var fabHappyPlace : FloatingActionButton = findViewById(R.id.fab_happy_place)
+
+        fabHappyPlace.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
