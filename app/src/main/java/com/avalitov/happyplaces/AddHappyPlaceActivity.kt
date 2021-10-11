c4c8e1350bdd2a3acbf958c5fc7ff122a3943ff1
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-10-11 21:09:08 +0500

    Add-Place Activity Layout is added

--- app/src/main/java/com/avalitov/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/avalitov/happyplaces/AddHappyPlaceActivity.kt
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 
+lateinit var toolbarAddPlace : Toolbar
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
 
-        var toolbarAddPlace : Toolbar = findViewById(R.id.tb_add_place)
+        toolbarAddPlace = findViewById(R.id.tb_add_place)
         setSupportActionBar(toolbarAddPlace)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
