b8631043b86cf41105a8a18ebd39de673176f493
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-10-22 19:24:14 +0500

    Taking images from Camera and Gallery realized

--- app/src/main/java/com/avalitov/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/avalitov/happyplaces/AddHappyPlaceActivity.kt
@@ -1,15 +1,19 @@
 package com.avalitov.happyplaces
 
 import android.Manifest
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -19,6 +23,8 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.IOError
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -26,6 +32,7 @@ import java.util.*
 lateinit var toolbarAddPlace : Toolbar
 lateinit var etDate : EditText
 lateinit var tvAddImage : TextView
+lateinit var ivPlaceImage : ImageView
 
 class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -58,6 +65,8 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         tvAddImage = findViewById(R.id.tv_add_image)
         tvAddImage.setOnClickListener(this)
 
+        ivPlaceImage = findViewById(R.id.iv_place_image)
+
 
 
     }
@@ -79,10 +88,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                 pictureDialog.setItems(pictureDialogItems) { dialog, which ->
                     when (which) {
                         0 -> choosePhotoFromGallery()
-                        1 -> Toast.makeText(
-                                this@AddHappyPlaceActivity,
-                                "Camera selection coming soon...",
-                                Toast.LENGTH_SHORT).show()
+                        1 -> takePhotoFromCamera()
                     }
                 }
                 pictureDialog.show()
@@ -90,18 +96,65 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun choosePhotoFromGallery() {
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                GALLERY_CODE -> {
+                    if (data != null) {
+                        val contentURI = data.data
+                        try {
+                            val selectedImageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+                            ivPlaceImage.setImageBitmap(selectedImageBitmap)
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                            Toast.makeText(this@AddHappyPlaceActivity,
+                                    "Failed to load an image from Gallery", Toast.LENGTH_SHORT)
+                                    .show()
+                        }
+                    }
+                }
+                CAMERA_CODE -> {
+                    val thumbnail : Bitmap = data?.extras?.get("data") as Bitmap
+                    ivPlaceImage.setImageBitmap(thumbnail)
+                }
+            }
+        }
+    }
+
+    //is written with third-party library DEXTER which helps to manage Permissions
+    private fun takePhotoFromCamera(){
         Dexter.withContext(this).withPermissions(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-                //Manifest.permission.CAMERA
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
         ).withListener(object : MultiplePermissionsListener {
 
             override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
                 if (report!!.areAllPermissionsGranted()) {
-                    Toast.makeText(this@AddHappyPlaceActivity,
-                    "Storage READ/WRITE permissions are granted. Now you can select image from Gallery",
-                    Toast.LENGTH_SHORT).show()
+                    val galleryIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(galleryIntent, CAMERA_CODE)
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken) {
+                showRationaleDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    //is written with third-party library DEXTER which helps to manage Permissions
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
+
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    val galleryIntent = Intent(Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    startActivityForResult(galleryIntent, GALLERY_CODE)
                 }
             }
 
@@ -135,5 +188,9 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         etDate.setText(sdf.format(cal.time))
     }
 
+    companion object {
+        private const val GALLERY_CODE = 1
+        private const val CAMERA_CODE = 2
+    }
 
 }
