2e1a465b6d47c49fc72201e5d4db2e06baa5aa5d
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-12-23 19:35:52 +0500

    RecyclerView displaying realized

--- app/src/main/java/com/avalitov/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/avalitov/happyplaces/activities/MainActivity.kt
@@ -3,11 +3,18 @@ package com.avalitov.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.avalitov.happyplaces.R
+import com.avalitov.happyplaces.adapters.HappyPlacesAdapter
 import com.avalitov.happyplaces.database.DatabaseHandler
 import com.avalitov.happyplaces.models.HappyPlaceModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+lateinit var rvHappyPlacesList : RecyclerView
+lateinit var tvNoPlacesFound : TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var fabHappyPlace : FloatingActionButton = findViewById(R.id.fab_happy_place)
+        rvHappyPlacesList = findViewById(R.id.rv_happy_places_list)
+        tvNoPlacesFound = findViewById(R.id.tv_no_places_found)
 
         fabHappyPlace.setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
@@ -25,17 +34,26 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    private fun setupHappyPlacesRecyclerView(happyPlacesList: ArrayList<HappyPlaceModel>){
+        rvHappyPlacesList.layoutManager = LinearLayoutManager(this)
+        rvHappyPlacesList.setHasFixedSize(true)
+
+        val placesAdapter = HappyPlacesAdapter(this, happyPlacesList)
+        rvHappyPlacesList.adapter = placesAdapter
+    }
+
+
     private fun getHappyPlacesListFromLocalDB() {
         val dbHandler = DatabaseHandler(this)
         val happyPlacesList : ArrayList<HappyPlaceModel> = dbHandler.getAllPlacesList()
 
-        //TODO: displaying via RecyclerView
-        //test output
         if(happyPlacesList.isNotEmpty()) {
-            for(place in happyPlacesList) {
-                Log.e("Title", place.title)
-                Log.e("Description", place.description)
-            }
+                rvHappyPlacesList.visibility = View.VISIBLE
+                tvNoPlacesFound.visibility = View.GONE
+                setupHappyPlacesRecyclerView(happyPlacesList)
+        } else {
+            rvHappyPlacesList.visibility = View.GONE
+            tvNoPlacesFound.visibility = View.VISIBLE
         }
     }
 }
---
+++ app/src/main/java/com/avalitov/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -0,0 +1,49 @@
+package com.avalitov.happyplaces.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.avalitov.happyplaces.R
+import com.avalitov.happyplaces.models.HappyPlaceModel
+
+open class HappyPlacesAdapter(
+    private val context : Context,
+    private val list: ArrayList<HappyPlaceModel>)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceViewHolder {
+        return PlaceViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.item_happy_place,
+                parent,
+                false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+        if(holder is PlaceViewHolder) {
+            holder.ivImage.setImageURI(Uri.parse(model.imagePath))
+            holder.tvTitle.text = model.title
+            holder.tvDescription.text = model.description
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+
+    class PlaceViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        var tvTitle: TextView = itemView.findViewById(R.id.tv_item_title)
+        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+        var ivImage: ImageView = itemView.findViewById(R.id.iv_item_place_image)
+    }
+
+
+}
