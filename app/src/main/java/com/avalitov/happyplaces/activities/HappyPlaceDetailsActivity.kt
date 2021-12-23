e173a1b176b121dc7e7b8ddfa5871f30a529d0c0
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-12-23 20:16:06 +0500

    HP Details Activity created

---
+++ app/src/main/java/com/avalitov/happyplaces/activities/HappyPlaceDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.avalitov.happyplaces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.avalitov.happyplaces.R
+
+class HappyPlaceDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_happy_place_details)
+    }
+}
